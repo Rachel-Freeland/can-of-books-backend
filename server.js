93c524b1a41d8caba0bae3b2f0c56056c887025d
ALEX GRAZDA <AGRAZDA@GMAIL.COM>
2021-08-30 16:38:37 -0700

    added mongoose

--- server.js
+++ server.js
@@ -3,12 +3,16 @@
 //----------------------Dependencies-----------------------
 const express = require('express');
 const cors = require('cors');
+// getting-started.js
+const mongoose = require('mongoose');
 
 //------------------------Configs--------------------------
 const app = express();
+main().catch(err => console.log(err));
 
 // Import and config the dotenv
 require('dotenv').config();
+
 
 // Designate the port
 const PORT = process.env.PORT;
@@ -23,6 +27,11 @@ app.get('/', (req, res) => {
 app.get('/*', (req, res) => {
   res.status(404).send('This is not the route you are looking for!');
 })
+
+//-------------------Functions and Classes--------------
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/test');
+}
 
 //------------------------Listening------------------------
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
