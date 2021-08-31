da0f1e59a0f70193fa9d63e7eb10b50d214ec87f
plaurion1989 <plaurion@protonmail.com>
2021-08-30 20:33:40 -0700

    saving books to db is working

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 //----------------------Dependencies-----------------------
 const express = require('express');
@@ -6,10 +6,16 @@ const cors = require('cors');
 
 // getting-started.js
 const mongoose = require('mongoose');
-mongoose.connect('http://localhost:3001');
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function () {
+  console.log('connected to mongo');
+});
 
 // Import BookSchema
-const Book = require('./Schemas/BookSchema.js');
+const Book = require('./BookSchema.js');
 
 //------------------------Configs--------------------------
 const app = express();
@@ -26,55 +32,50 @@ const PORT = process.env.PORT;
 // Set up the primary "root" route
 app.get('/', (req, res) => {
   res.send('TESTING!!');
-})
+});
 
 app.get('/books', (req, res) => {
   res.status(200).send(Book);
-})
+});
 
 // Default route to catch any other routes that may be entered
 app.get('/*', (req, res) => {
   res.status(404).send('This is not the route you are looking for!');
-})
+});
 
 //-------------------Functions and Classes--------------
 
 
 //------------------------Seeding DB------------------------
-async function seed() {
-
+async function createBook(){
   const Book1 = new Book({
     title: 'Little House on the Prarie',
     description: 'The trials and tribulations of a young girl on the prarie',
     status: 'available',
     email: 'littlejohn@aol.com'
-  })
-
-  
-  await Book1.save( function(err) {
-    if (err) console.log('You screwed up!');
-    else console.log('You ROCK!');
   });
 
-
-  await Book.create({
+  const Book2 = new Book({
     title: 'Little Women',
     description: 'The trials and tribulations of a young girl',
     status: 'available',
     email: 'littlejohn@aol.com'
-  })
-
-  await Book.create({
+  });
+  const Book3 = new Book({
     title: 'Little Luna',
     description: 'The trials and tribulations of a little dog',
     status: 'available',
     email: 'littlejohn@aol.com'
-  })
-
+  });
+  await Book1.save();
+  await Book2.save();
+  await Book3.save();
+  console.log('books saved');
   mongoose.disconnect();
 }
 
-seed();
+createBook();
+
 
 //------------------------Listening------------------------
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
